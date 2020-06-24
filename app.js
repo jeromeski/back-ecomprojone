93aee37e11c9e3a5b09ab50fa3b4c50dcadadf64
Jeromeski <jeromegee@gmail.com>
2020-06-24 08:25:06 +0800

    Connecting to MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express');
-const app = express();
+
+const mongoose = require('mongoose');
 require('dotenv').config()
 
+// app
+const app = express();
+
+
+// db
+mongoose.connect(
+  process.env.DATABASE, {useNewUrlParser: true, useCreateIndex: true}
+).then(() => console.log('DB Connected'))
+
+mongoose.connection.on('error', err => {
+  console.log('DB connection error: ${err.message}')
+})
+
+// routes
 app.get('/', (req, res) => {
   res.send('hello from node')
 });
