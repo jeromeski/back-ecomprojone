0ea6dd135d526d7aedb50d9ff696ee33514d3fcf
Jeromeski <jeromegee@gmail.com>
2020-06-24 09:07:57 +0800

    Setup Controllers

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { sayHi } = require('../controllers/user');
 
-router.get('/', (req, res) => {
-  res.send('hello from node: test123');
-});
+router.get('/', sayHi);
 
 module.exports = router;
