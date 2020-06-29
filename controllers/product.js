d5ca89faf8f0164e5a1ea502168bfc719ecb733d
Jeromeski <jeromegee@gmail.com>
2020-06-29 11:27:30 +0800

    Adding updating product feature

--- controllers/product.js
+++ controllers/product.js
@@ -4,7 +4,6 @@ const _ = require('lodash');
 const Product = require('../models/product');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
-
 exports.productById = (req, res, next, id) => {
   Product.findById(id).exec((err, product) => {
     if (err || !product) {
@@ -19,6 +18,7 @@ exports.productById = (req, res, next, id) => {
 };
 
 exports.read = (req, res) => {
+  console.log(req)
   req.product.photo = undefined;
   return res.json(req.product);
 };
@@ -51,6 +51,77 @@ exports.create = (req, res) => {
     }
 
     let product = new Product(fields);
+    if (files.photo) {
+      // console.log('Files Photo: ', files.photo);
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: 'Image should be less than 1mb in size'
+        });
+      }
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentyType = files.photo.type;
+    }
+
+    product.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err)
+        });
+      }
+      res.json(result);
+    });
+  });
+};
+
+exports.remove = (req, res) => {
+  let product = req.product;
+  product.remove((err, deletedProduct) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err)
+      });
+    }
+    res.json({
+      deletedProduct,
+      message: 'Product deleted successfully'
+    });
+  });
+};
+
+exports.update = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: 'Image could not be uploaded'
+      });
+    }
+
+    // check for all fields
+
+    const { name, description, price, category, quantity, shipping } = fields;
+
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: 'All fields are required'
+      });
+    }
+
+    let product = req.product
+
+    product = _.extend(product, fields)
+
+    // 1kb = 1000
+    // 1mb = 1000000
+
     if (files.photo) {
       // console.log('Files Photo: ', files.photo);
       if (files.photo.size > 1000000) {
