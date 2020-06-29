9a2269c3357a1dd25c7a8e1b87312463d54dfab6
Jeromeski <jeromegee@gmail.com>
2020-06-29 18:10:07 +0800

    Fetch products based on request query using postman

--- controllers/product.js
+++ controllers/product.js
@@ -154,14 +154,14 @@ exports.update = (req, res) => {
 exports.list = (req, res) => {
   let order = req.query.order ? req.query.order : 'asc';
   let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
-  let limit = req.query.limit ? req.query.limit : 6;
+  let limit = req.query.limit ? parseInt(req.query.limit) : 6;
 
   Product.find()
     .select('-photo')
     .populate('category')
     .sort([[sortBy, order]])
     .limit(limit)
-    .exec((err, data) => {
+    .exec((err, products) => {
       if (err) {
         return res.status(400).json({
           error: 'Products not found'
