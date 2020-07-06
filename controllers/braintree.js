6ad4c41da254c2ff3b476893f7706b6ff122daa2
Jeromeski <jeromegee@gmail.com>
2020-07-06 18:57:02 +0800

    Processing payment backend

--- controllers/braintree.js
+++ controllers/braintree.js
@@ -2,8 +2,6 @@ const User = require('../models/user');
 const braintree = require('braintree');
 require('dotenv').config();
 
-
-
 const gateway = braintree.connect({
   environment: braintree.Environment.Sandbox,
   publicKey: process.env.BRAINTREE_PUBLIC_KEY,
@@ -11,14 +9,35 @@ const gateway = braintree.connect({
   merchantId: process.env.BRAINTREE_MERCHANT_ID
 });
 
-
-
-exports.generateToken = (req,res) => {
+exports.generateToken = (req, res) => {
   gateway.clientToken.generate({}, function(err, response) {
-    if(err) {
-      res.status(500).send(err)
+    if (err) {
+      res.status(500).send(err);
     } else {
-      res.send(response)
+      res.send(response);
     }
-  })
-}
+  });
+};
+
+exports.processPayment = (req, res) => {
+  let nonceFromTheClient = req.body.paymentMethodNonce;
+  let amountFromTheClient = req.body.amount;
+
+  // charge
+  let newTransaction = gateway.transaction.sale(
+    {
+      amount: amountFromTheClient,
+      paymentMethodNonce: nonceFromTheClient,
+      options: {
+        submitForSettlement: true
+      }
+    },
+    (error, result) => {
+      if (error) {
+        res.status(500).json(error);
+      } else {
+        res.json(result);
+      }
+    }
+  );
+};
